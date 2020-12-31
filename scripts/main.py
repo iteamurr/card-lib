41a125ec12158e8b8e7c0d5b553378e4b79daa74
iteamurr <iteamurr@gmail.com>
2020-12-31 21:12:25 +0300

    Saving JSON file

--- scripts/main.py
+++ scripts/main.py
@@ -1,15 +1,14 @@
-# Other imports
-import json
-import requests
-
 # Flask
 from flask import Flask
 from flask import request
 from flask import jsonify
 
-# Project imports
-import config
+# Other imports
+import json
 
+# Project imports
+import bot
+import config
 
 app = Flask(__name__)
 
@@ -18,25 +17,17 @@ app = Flask(__name__)
 def get_new_message():
     if request.method == 'POST':
         JSON_result = request.get_json()
-        message_handler(JSON_result)
+
+        save_message(JSON_result) # Debug
+        bot.message_handler(JSON_result)
+
         return jsonify(JSON_result)
-    return 'Error'
+    return '<h1>Error!</h1>'
 
 
-@app.route('/')
-def index():
-    return '<h1>Hello, World!</h1>'
-
-
-def message_handler(JSON):
-    chat_id = JSON['result'][0]['message']['chat']['id']
-
-    URL = f'https://api.telegram.org/bot{config.TOKEN}/sendMessage'
-    payload = {
-        'chat_id': chat_id,
-        'text': 'Hello, World!'
-    }
-    requests.post(URL, data=payload)
+def save_message(JSON):
+    with open('message.json', 'w') as JSON_file:
+        json.dump(JSON, JSON_file, indent=4)
 
 
 if __name__ == '__main__':
