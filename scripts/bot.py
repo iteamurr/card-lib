c7ac395f8b002d35858b6e018854f87640e554e3
iteamurr <iteamurr@gmail.com>
2021-01-03 11:20:56 +0300

    Change of structure

--- scripts/bot.py
+++ scripts/bot.py
@@ -7,19 +7,24 @@ import tools
 
 
 def message_handler(JSON):
-    message_chat_id = JSON['message']['chat']['id']
-    message_text = JSON['message']['text']
+    message = JSON['message']
+    message_text = message['text']
+    message_chat_id = message['chat']['id']
 
-    if 'entities' in JSON['message']:
-        if JSON['message']['entities'][0]['type'] == 'bot_command':
+    if 'entities' in message:
+        message_entities = message['entities']
+
+        if message_entities[0]['type'] == 'bot_command':
             if '/start' in message_text:
                 send_profile(message_chat_id, 'Личный кабинет')
 
 
 def send_profile(chat_id, text):
     url, response = tools.response_creator('sendMessage', chat_id, text)
-    keyboard = tools.inline_keyboard_creator(('Коллекции', 'collections'),
-                                             ('Настройки', 'settings'))
+    keyboard = tools.inline_keyboard_creator(
+        ('Коллекции', 'collections'),
+        ('Настройки', 'settings')
+    )
     data = {**response, **keyboard}
 
     requests.post(url, json=data)
--- scripts/tools.py
+++ scripts/tools.py
@@ -3,6 +3,25 @@ import config
 
 
 def response_creator(command, chat_id, text):
+    '''Create a request wrapper.
+
+    Parameters
+    ----------
+    command : str
+        Command name from Telegram documentation.
+    chat_id : str
+        Unique user id.
+    text : str
+        The text to be wrapped.
+
+    Returns
+    -------
+    url : str
+        Link to send a message.
+    JSON_response : dict
+        Extending the message.
+    '''
+
     url = config.URL.format(token=config.TOKEN, command=command)
     JSON_response = {
         'chat_id': chat_id,
@@ -14,6 +33,19 @@ def response_creator(command, chat_id, text):
 
 
 def inline_keyboard_creator(*button_data):
+    '''Inline keyboard creation.
+
+    Parameters
+    ----------
+    *button_data
+        Button data to wrap.
+
+    Returns
+    -------
+    keyboard : dict
+        Ready wrapped keyboard.
+    '''
+
     keyboard = {
         'reply_markup': 
             {
