80f52d7a1885c536f739d66e6f52a57f25ccbed1
iteamurr <iteamurr@gmail.com>
2021-01-04 13:07:39 +0300

    Added database access constants

--- scripts/config_example.py
+++ scripts/config_example.py
@@ -1,5 +1,8 @@
 TOKEN = '123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11'
 URL = 'https://api.telegram.org/bot{token}/{command}'
 
+DB_USER = 'postgres'
+DB_PASSWORD = 'postgres'
+
 # Webhook registration
 # https://api.telegram.org/bot<TOKEN>/setWebhook?url=https://www.example.com/<TOKEN>
