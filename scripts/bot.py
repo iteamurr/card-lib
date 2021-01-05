807f9b67972e6ba28d8b9ed02737d7611b47b96c
iteamurr <iteamurr@gmail.com>
2021-01-05 11:55:43 +0300

    Changed MessageHandler class

--- scripts/bot.py
+++ scripts/bot.py
@@ -7,26 +7,57 @@ import config
 import database as db
 
 
-def message_handler(JSON):
-    '''Checking a message for a request type.
+class MessageHandler:
+    def __init__(self, JSON):
+        self.JSON = JSON
 
-    Parameters
-    ----------
-    JSON : dict
-        A key unique to each message.
-    '''
+    def handler(self):
+        '''Checking a message for a request type.
+        '''
 
-    message = JSON['message']
-    message_text = message['text']
-    chat_id = message['chat']['id']
+        message = self.JSON['message']
+        message_text = message['text']
+        chat_id = message['chat']['id']
 
-    send_menu = SendMenu(chat_id)
-    if 'entities' in message:
-        message_entities = message['entities']
+        send_menu = SendMenu(chat_id)
+        if 'entities' in message:
+            message_entities = message['entities']
 
-        if message_entities[0]['type'] == 'bot_command':
-            if '/start' in message_text:
-                send_menu.private_office()
+            if message_entities[0]['type'] == 'bot_command':
+                if '/start' in message_text:
+                    self.check_new_user()
+                    send_menu.private_office()
+
+
+    def check_new_user(self):
+        '''Checking for the presence of a user in the database.
+        If there is no user, write it to the database.
+        '''
+
+        message = self.JSON['message']
+        chat_id = message['chat']['id']
+
+        db_select = db.Select(
+            name='bot_users',
+            user=config.DB_USER, password=config.DB_PASSWORD,
+            host=config.HOST, port=config.PORT)
+
+        user_existence = db_select.get_user_attributes(chat_id)
+
+        if not user_existence:
+            user_menu_id = message['message_id']
+            username = message['from']['username']
+            locale = message['from']['language_code']
+            user_locale = locale if locale in ['en', 'ru'] else 'en'
+
+            db_insert = db.Insert(
+                name='bot_users',
+                user=config.DB_USER, password=config.DB_PASSWORD,
+                host=config.HOST, port=config.PORT)
+
+            db_insert.new_user(
+                user_id=chat_id, username=username,
+                locale=user_locale, menu_id=user_menu_id)
 
 
 class SendMenu:
@@ -37,15 +68,30 @@ class SendMenu:
         '''Sending a personal user menu.
         '''
 
-        db_select = db.Select(
+        db_select_user_locale = db.Select(
+            name='bot_users',
             user=config.DB_USER, password=config.DB_PASSWORD,
-            name='bot_messages')
-        menu_text = db_select.get_bot_message('private_office', 'en')
-        settings_menu_name = db_select.get_bot_message('settings', 'en')
-        collections_menu_name = db_select.get_bot_message('collections', 'en')
+            host=config.HOST, port=config.PORT)
+
+        locale = db_select_user_locale.get_user_attributes(self.chat_id)[3]
+
+        db_select_message = db.Select(
+            name='bot_messages',
+            user=config.DB_USER, password=config.DB_PASSWORD,
+            host=config.HOST, port=config.PORT)
+
+        menu_text = db_select_message.get_bot_message(
+            data='private_office', locale=locale)
+
+        settings_menu_name = db_select_message.get_bot_message(
+            data='settings', locale=locale)
+
+        collections_menu_name = db_select_message.get_bot_message(
+            data='collections', locale=locale)
 
         url, response = tools.response_creator(
             command='sendMessage', chat_id=self.chat_id, text=menu_text)
+
         keyboard = tools.inline_keyboard_creator(
             (collections_menu_name, 'collections'),
             (settings_menu_name, 'settings')
