a7121062a4be42e0fc4b5fb603ab3fd8576967ed
iteamurr <iteamurr@gmail.com>
2021-01-05 11:54:52 +0300

    Added standard HOST and PORT

--- scripts/config_example.py
+++ scripts/config_example.py
@@ -3,6 +3,8 @@ URL = 'https://api.telegram.org/bot{token}/{command}'
 
 DB_USER = 'postgres'
 DB_PASSWORD = 'postgres'
+HOST = 'localhost'
+PORT = '5432'
 
 # Webhook registration
 # https://api.telegram.org/bot<TOKEN>/setWebhook?url=https://www.example.com/<TOKEN>
