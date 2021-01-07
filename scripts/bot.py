aaf90f54313e71d0f156bc1227044a8a8aa3b2f6
iteamurr <iteamurr@gmail.com>
2021-01-07 13:25:13 +0300

    Added callback query processing

--- scripts/bot.py
+++ scripts/bot.py
@@ -7,34 +7,73 @@ import config
 import database as db
 
 
-class MessageHandler:
-    def __init__(self, JSON):
-        self.JSON = JSON
-
-    def handler(self):
+class Handler:
+    def handler(self, JSON):
         '''Checking a message for a request type.
+
+        Parameters
+        ----------
+        JSON : JSON
+            This object represents a message.
         '''
 
-        message = self.JSON['message']
-        message_text = message['text']
-        chat_id = message['chat']['id']
+        if 'message' in JSON:
+            message = JSON['message']
+            self.message_handler(message)
 
-        send_menu = SendMenu(chat_id)
+        elif 'callback_query' in JSON:
+            callback_query = JSON['callback_query']
+            self.callback_query_handler(callback_query)
+
+    def message_handler(self, message):
+        '''Work with a simple message.
+
+        Parameters
+        ----------
+        message : JSON
+            Message content.
+        '''
+
+        chat_id = message['chat']['id']
+        message_text = message['text']
+
+        send_menu = Menu(chat_id)
         if 'entities' in message:
             message_entities = message['entities']
 
             if message_entities[0]['type'] == 'bot_command':
                 if '/start' in message_text:
-                    self.check_new_user()
+                    self._check_new_user(message)
                     send_menu.private_office()
 
+    def callback_query_handler(self, callback_query):
+        '''Work with inline message.
 
-    def check_new_user(self):
-        '''Checking for the presence of a user in the database.
-        If there is no user, write it to the database.
+        Parameters
+        ----------
+        callback_query : JSON
+            An object containing information
+            about the pressed button.
         '''
 
-        message = self.JSON['message']
+        data = callback_query['data']
+        chat_id = callback_query['from']['id']
+        message_id = callback_query['message']['message_id']
+
+        switch_menu = Menu(chat_id)
+        if data == 'settings':
+            switch_menu.settings(message_id)
+
+    def _check_new_user(self, message):
+        '''Checking for the presence of a user in the database.
+        If there is no user, write it to the database.
+
+        Parameters
+        ----------
+        message : JSON
+            Message content.
+        '''
+
         chat_id = message['chat']['id']
 
         db_select = db.Select(
@@ -60,12 +99,68 @@ class MessageHandler:
                 locale=user_locale, menu_id=user_menu_id)
 
 
-class SendMenu:
+class Menu:
     def __init__(self, chat_id):
         self.chat_id = chat_id
 
     def private_office(self):
         '''Sending a personal user menu.
+        '''
+
+        locale = self._get_user_locale()
+        menu, collections, settings = self._get_menu_name(
+            locale, 'private_office', 'collections', 'settings')
+
+        url, response = tools.response_creator(
+            command='sendMessage', chat_id=self.chat_id, text=menu)
+
+        keyboard = tools.inline_keyboard_creator(
+            [
+                [collections, 'collections'],
+                [settings, 'settings']
+            ]
+        )
+
+        data = {**response, **keyboard}
+        requests.post(url, json=data)
+
+    def settings(self, message_id):
+        '''Changing the current menu to the settings menu.
+
+        Parameters
+        ----------
+        message_id : int
+            Unique message id.
+        '''
+
+        locale = self._get_user_locale()
+        menu, locale_settings, back = self._get_menu_name(
+            locale, 'settings', 'locale_settings', 'back')
+
+        url, response = tools.response_creator(
+            command='editMessageText',
+            chat_id=self.chat_id, text=menu,
+            message_id=message_id)
+
+        keyboard = tools.inline_keyboard_creator(
+            [
+                [locale_settings, 'locale_settings']
+            ],
+            [
+                [back, 'private_office']
+            ]
+        )
+
+        data = {**response, **keyboard}
+        requests.post(url, json=data)
+
+    def _get_user_locale(self):
+        '''Getting user language settings.
+
+        Returns
+        -------
+        locale : str
+            User locale.
         '''
 
         db_select_user_locale = db.Select(
@@ -75,27 +170,32 @@ class SendMenu:
 
         locale = db_select_user_locale.user_attributes(self.chat_id)[3]
 
+        return locale
+
+    def _get_menu_name(self, locale, *menu_list):
+        '''Retrieving menu names from the database.
+
+        Parameters
+        ----------
+        locale : str
+            A key unique to each message.
+        menu_list
+            List of menus to get names.
+
+        Returns
+        -------
+        menu_names : list
+            List of menu names.
+        '''
+
         db_select_message = db.Select(
             name='bot_messages',
             user=config.DB_USER, password=config.DB_PASSWORD,
             host=config.HOST, port=config.PORT)
 
-        menu_text = db_select_message.bot_message(
-            data='private_office', locale=locale)
+        menu_names = []
+        for data in menu_list:
+            menu_name = db_select_message.bot_message(data, locale)
+            menu_names.append(menu_name)
 
-        settings_menu_name = db_select_message.bot_message(
-            data='settings', locale=locale)
-
-        collections_menu_name = db_select_message.bot_message(
-            data='collections', locale=locale)
-
-        url, response = tools.response_creator(
-            command='sendMessage', chat_id=self.chat_id, text=menu_text)
-
-        keyboard = tools.inline_keyboard_creator(
-            (collections_menu_name, 'collections'),
-            (settings_menu_name, 'settings')
-        )
-
-        data = {**response, **keyboard}
-        requests.post(url, json=data)
+        return menu_names
