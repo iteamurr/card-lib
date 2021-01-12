59983be81873fc244dfdc7155cf174a19fb35365
iteamurr <iteamurr@gmail.com>
2021-01-12 13:18:54 +0300

    style: change code style

--- scripts/main.py
+++ scripts/main.py
@@ -1,36 +1,27 @@
-# -*- coding: utf-8 -*-
-'''Program entry point.
+# pylint: disable=import-error
+# pylint: disable=missing-module-docstring
+# pylint: disable=missing-function-docstring
 
-Module receives messages from the Telegram
-and passes further instructions to the bot.py module.
-'''
-
-# Other imports
 import json
 
-# Flask
 from flask import Flask
 from flask import request
 from flask import jsonify
 
-# Project imports
 import bot
 
 
 app = Flask(__name__)
 
 
-with open('config.json') as config_json:
+with open("config.json") as config_json:
     config = json.load(config_json)
-    token = config['telegram']['token']
+    token = config["telegram"]["token"]
 
 
-@app.route(f'/{token}', methods=['POST', 'GET'])
+@app.route(f"/{token}", methods=["POST", "GET"])
 def get_new_message():
-    '''Receiving a message from a user using a webhook.
-    '''
-
-    if request.method == 'POST':
+    if request.method == "POST":
         json_result = request.get_json()
 
         save_message(json_result) # Debug
@@ -38,16 +29,13 @@ def get_new_message():
         bot_handler.handler(json_result)
 
         return jsonify(json_result)
-    return '<h1>Error!</h1>'
+    return "<h1>Error!</h1>"
 
 
 def save_message(json_result):
-    '''Debug function.
-    '''
-
-    with open('message.json', 'w') as json_result_file:
+    with open("message.json", "w") as json_result_file:
         json.dump(json_result, json_result_file, indent=4)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
