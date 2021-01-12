29f76f9bbed84bbcc6bfab96b06003524498491d
iteamurr <iteamurr@gmail.com>
2021-01-12 13:17:01 +0300

    feat: add Update class

--- scripts/database.py
+++ scripts/database.py
@@ -4,6 +4,7 @@
 
 import json
 import psycopg2
+from psycopg2 import sql
 
 
 class CreateTable:
@@ -175,3 +176,45 @@ class Select:
 
         attributes = self._cursor.fetchone()
         return attributes
+
+
+class Update:
+    def __init__(self, db_name):
+        self._db_name = db_name
+        self._connection = None
+        self._cursor = None
+
+        with open("config.json") as config_json:
+            config = json.load(config_json)
+
+            self._user = config["database"]["user"]
+            self._passwd = config["database"]["passwd"]
+
+            self._host = config["database"]["host"]
+            self._port = config["database"]["port"]
+
+    def __enter__(self):
+        self._connection = psycopg2.connect(
+            dbname=self._db_name,
+            user=self._user, password=self._passwd,
+            host=self._host, port=self._port
+        )
+        self._cursor = self._connection.cursor()
+
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        if traceback is None:
+            self._connection.commit()
+        else:
+            self._connection.rollback()
+
+        self._cursor.close()
+        self._connection.close()
+
+    def user_attribute(self, user_id, attribute, value):
+        self._cursor.execute(
+            sql.SQL(
+                "UPDATE users SET {}=%s WHERE user_id=%s"
+            ).format(sql.Identifier(attribute)), (value, user_id)
+        )
