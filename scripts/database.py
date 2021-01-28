0a4d93b4671c91b2367370f6977f385ae4383c8f
iteamurr <iteamurr@gmail.com>
2021-01-28 22:14:08 +0300

    docs: update database documentation

--- scripts/database.py
+++ scripts/database.py
@@ -1,32 +1,27 @@
-# pylint: disable=missing-module-docstring
-# pylint: disable=missing-class-docstring
-# pylint: disable=missing-function-docstring
+"""
+    Database module.
+"""
 
-import json
 import psycopg2
 from psycopg2 import sql
 
+from .config import database
+
 
 class CreateTable:
+    """Class responsible for creating tables in the database.
+    """
+
     def __init__(self, db_name):
         self.db_name = db_name
         self._connection = None
         self._cursor = None
 
-        with open("config.json") as config_json:
-            config = json.load(config_json)
-
-            self._user = config["database"]["user"]
-            self._passwd = config["database"]["passwd"]
-
-            self._host = config["database"]["host"]
-            self._port = config["database"]["port"]
-
     def __enter__(self):
         self._connection = psycopg2.connect(
             dbname=self.db_name,
-            user=self._user, password=self._passwd,
-            host=self._host, port=self._port
+            user=database["user"], password=database["passwd"],
+            host=database["host"], port=database["port"]
         )
         self._cursor = self._connection.cursor()
 
@@ -42,6 +37,9 @@ class CreateTable:
         self._connection.close()
 
     def bot_messages(self):
+        """Create a bot message table.
+        """
+
         self._cursor.execute(
             """CREATE TABLE messages (
                id serial PRIMARY KEY,
@@ -53,6 +51,9 @@ class CreateTable:
         )
 
     def bot_users(self):
+        """Create a bot users table.
+        """
+
         self._cursor.execute(
             """CREATE TABLE users (
                id serial PRIMARY KEY,
@@ -69,6 +70,9 @@ class CreateTable:
         )
 
     def bot_collections(self):
+        """Create a bot user collections table.
+        """
+
         self._cursor.execute(
             """CREATE TABLE collections (
                id serial PRIMARY KEY,
@@ -83,25 +87,19 @@ class CreateTable:
 
 
 class Insert:
+    """Class responsible for writing new data to the database.
+    """
+
     def __init__(self, db_name):
         self._db_name = db_name
         self._connection = None
         self._cursor = None
 
-        with open("config.json") as config_json:
-            config = json.load(config_json)
-
-            self._user = config["database"]["user"]
-            self._passwd = config["database"]["passwd"]
-
-            self._host = config["database"]["host"]
-            self._port = config["database"]["port"]
-
     def __enter__(self):
         self._connection = psycopg2.connect(
             dbname=self._db_name,
-            user=self._user, password=self._passwd,
-            host=self._host, port=self._port
+            user=database["user"], password=database["passwd"],
+            host=database["host"], port=database["port"]
         )
         self._cursor = self._connection.cursor()
 
@@ -117,6 +115,20 @@ class Insert:
         self._connection.close()
 
     def new_bot_message(self, data, message, locale="en"):
+        """Insert a new bot message.
+
+        Parameters
+        ----------
+        data : str
+            Unique message identifier.
+        message : str
+            Message text.
+        locale : str, optional
+            A variable defining the user's language
+            and any special preferences
+            that the user wants to see in their user interface.
+        """
+
         self._cursor.execute(
             """INSERT INTO messages (locale, data, message)
                VALUES (%s, %s, %s);
@@ -124,6 +136,22 @@ class Insert:
         )
 
     def new_user(self, user_id, username, locale, menu_id):
+        """Register new user.
+
+        Parameters
+        ----------
+        user_id : int
+            Unique identifier of the target user.
+        username : str
+            User's username.
+        locale : str
+            A variable defining the user's language
+            and any special preferences
+            that the user wants to see in their user interface.
+        menu_id : int
+            Unique menu identifier.
+        """
+
         self._cursor.execute(
             """INSERT INTO users (
                user_id,
@@ -139,6 +167,18 @@ class Insert:
         )
 
     def new_collection(self, user_id, key, name):
+        """Insert a new collection.
+
+        Parameters
+        ----------
+        user_id : int
+            Unique identifier of the target user.
+        key : str
+            Unique identifier for the collection.
+        name : str
+            Collection name.
+        """
+
         self._cursor.execute(
             """INSERT INTO collections (
                user_id,
@@ -152,25 +192,19 @@ class Insert:
 
 
 class Select:
+    """Class responsible for retrieving information from the database.
+    """
+
     def __init__(self, db_name):
         self._db_name = db_name
         self._connection = None
         self._cursor = None
 
-        with open("config.json") as config_json:
-            config = json.load(config_json)
-
-            self._user = config["database"]["user"]
-            self._passwd = config["database"]["passwd"]
-
-            self._host = config["database"]["host"]
-            self._port = config["database"]["port"]
-
     def __enter__(self):
         self._connection = psycopg2.connect(
             dbname=self._db_name,
-            user=self._user, password=self._passwd,
-            host=self._host, port=self._port
+            user=database["user"], password=database["passwd"],
+            host=database["host"], port=database["port"]
         )
         self._cursor = self._connection.cursor()
 
@@ -186,6 +220,23 @@ class Select:
         self._connection.close()
 
     def bot_message(self, data, locale="en"):
+        """Get bot message.
+
+        Parameters
+        ----------
+        data : str
+            Unique message identifier.
+        locale : str, optional
+            A variable defining the user's language
+            and any special preferences
+            that the user wants to see in their user interface.
+
+        Returns
+        -------
+        message : str
+            Bot message if successful, None otherwise.
+        """
+
         self._cursor.execute(
             """SELECT message FROM messages
                WHERE locale=%s AND
@@ -199,6 +250,21 @@ class Select:
         return None
 
     def user_attribute(self, user_id, attribute):
+        """Get user attribute.
+
+        Parameters
+        ----------
+        user_id : int
+            Unique identifier of the target user.
+        attribute : str
+            The name of the attribute whose value you want to get.
+
+        Returns
+        -------
+        attribute_value : str or int
+            Attribute value if successful, None otherwise.
+        """
+
         self._cursor.execute(
             sql.SQL(
                 "SELECT {} FROM users WHERE user_id=%s;"
@@ -210,16 +276,24 @@ class Select:
             return attribute_value[0]
         return None
 
-    def user_collections(self, user_id):
-        self._cursor.execute(
-            """SELECT * FROM collections WHERE user_id=%s;
-            """, (user_id,)
-        )
-
-        collections = self._cursor.fetchall()
-        return collections
-
     def collection_attribute(self, user_id, key, attribute):
+        """Get collection attribute.
+
+        Parameters
+        ----------
+        user_id : int
+            Unique identifier of the target user.
+        key : str
+            Unique identifier for the collection.
+        attribute : str
+            The name of the attribute whose value you want to get.
+
+        Returns
+        -------
+        attribute_value : str or int
+            Attribute value if successful, None otherwise.
+        """
+
         self._cursor.execute(
             sql.SQL(
                 "SELECT {} FROM collections WHERE user_id=%s AND key=%s;"
@@ -231,27 +305,43 @@ class Select:
             return attribute_value[0]
         return None
 
+    def user_collections(self, user_id):
+        """Get all user collections.
+
+        Parameters
+        ----------
+        user_id : int
+            Unique identifier of the target user.
+
+        Returns
+        -------
+        collections : list
+            All user collections.
+        """
+
+        self._cursor.execute(
+            """SELECT * FROM collections WHERE user_id=%s;
+            """, (user_id,)
+        )
+
+        collections = self._cursor.fetchall()
+        return collections
+
 
 class Update:
+    """Class responsible for updating data in the database.
+    """
+
     def __init__(self, db_name):
         self._db_name = db_name
         self._connection = None
         self._cursor = None
 
-        with open("config.json") as config_json:
-            config = json.load(config_json)
-
-            self._user = config["database"]["user"]
-            self._passwd = config["database"]["passwd"]
-
-            self._host = config["database"]["host"]
-            self._port = config["database"]["port"]
-
     def __enter__(self):
         self._connection = psycopg2.connect(
             dbname=self._db_name,
-            user=self._user, password=self._passwd,
-            host=self._host, port=self._port
+            user=database["user"], password=database["passwd"],
+            host=database["host"], port=database["port"]
         )
         self._cursor = self._connection.cursor()
 
@@ -267,6 +357,18 @@ class Update:
         self._connection.close()
 
     def user_attribute(self, user_id, attribute, value):
+        """Update user attribute value.
+
+        Parameters
+        ----------
+        user_id : int
+            Unique identifier of the target user.
+        attribute : str
+            The name of the attribute whose value you want to update.
+        value : str or int
+            New attribute value.
+        """
+
         self._cursor.execute(
             sql.SQL(
                 "UPDATE users SET {}=%s WHERE user_id=%s;"
@@ -274,6 +376,20 @@ class Update:
         )
 
     def collection_attribute(self, user_id, key, attribute, value):
+        """Update collection attribute value.
+
+        Parameters
+        ----------
+        user_id : int
+            Unique identifier of the target user.
+        key : str
+            Unique identifier for the collection.
+        attribute : str
+            The name of the attribute whose value you want to update.
+        value : str or int
+            New attribute value.
+        """
+
         self._cursor.execute(
             sql.SQL(
                 "UPDATE collections SET {}=%s WHERE user_id=%s AND key=%s;"
