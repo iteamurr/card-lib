6c530e8683f0a72e00843a0ffbd49e9e9693a1ed
iteamurr <iteamurr@gmail.com>
2021-03-03 22:29:00 +0300

    feat: add send collections command

--- scripts/bot/menu.py
+++ scripts/bot/menu.py
@@ -3,12 +3,16 @@
 """
 
 from typing import Any
+from typing import Optional
 
 from ..tools import API
+from ..tools import Tools
 from ..database import Select
 from ..shortcuts import MenuTemplates
+from ..shortcuts import CollectionTemplates
 
 
+# pylint: disable=unsubscriptable-object
 class SendMenu:
     """Sending basic user menus.
     """
@@ -44,6 +48,38 @@ class SendMenu:
         menu = MenuTemplates.settings_template(locale)
         API.send_message(
             self.user_id, title, keyboard=API.inline_keyboard(menu)
+        )
+
+    def collections(self, per_page: Optional[int] = 8) -> None:
+        """Send all user collections.
+
+        Args:
+            per_page: Number of collections per page.
+        """
+
+        with Select("bot_users") as select:
+            locale = select.user_attribute(self.user_id, "locale")
+            level = select.user_attribute(self.user_id, "page_level")
+
+        with Select("bot_messages") as select:
+            title = select.bot_message("collections", locale)
+
+        with Select("bot_collections") as select:
+            collections_list = select.user_collections(self.user_id)
+
+        navigation = Tools.navigation_creator(
+            "CoLsSe", len(collections_list), level=level, per_page=per_page
+        )
+        items = collections_list[per_page*level:per_page*(level + 1)]
+        collection_buttons = Tools.button_list_creator(
+            "collection", "CoLSe", "info", items
+        )
+        buttons = CollectionTemplates.collections_template(locale)
+        menu = (navigation + collection_buttons + buttons)
+
+        API.send_message(
+            self.user_id, title,
+            keyboard=API.inline_keyboard(menu)
         )
 
 
