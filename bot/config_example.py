92915b75f200aefb93d8849a0ec9a7aa751e4ef9
iteamurr <iteamurr@gmail.com>
2021-03-16 23:11:43 +0300

    refactor: update example config

---
+++ bot/config_example.py
@@ -0,0 +1,20 @@
+"""
+    Bot configuration module.
+"""
+
+telegram = {
+    "token": "123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11",
+    "url": "https://api.telegram.org/bot{}/{}"
+}
+
+database = {
+    "host": "localhost",
+    "port": 5432,
+    "user": "postgres",
+    "passwd": "postgres"
+}
+
+bot_settings = {
+    "collections_per_page": 8,
+    "cards_per_page": 8
+}
